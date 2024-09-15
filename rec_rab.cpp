dcfeae96e8536d0351f58bb0eae42b0c2f4db33a
Taly Adler <redacted>
2024-09-15 19:01:43 +0300

    rabits working

--- rec_rab.cpp
+++ rec_rab.cpp
@@ -2,13 +2,25 @@
 
 int bunnyEars(int bunnies) {
 	printf("this is round %d\n", bunnies);
-	if (bunnies == 1) {
+	if (bunnies == 0){ 
+		return 0;
+	}
+	return 2 + bunnyEars(bunnies-1);
+}
+
+int oddEars(int e) {
+	if (e == 1){
+		printf("adding %d ears\n", e*3);
 		return 1;
 	}
-	int x =  bunnyEars(bunnies-1);
-	return x * 2;
+	else if (e % 2 == 0){
+		printf("adding %d ears\n", (e/e)+1);
+		return oddEars(e-1)*2;
+	}
+	printf("adding %d ears\n", (e/e)+2);
+	return  oddEars(e-1)*3;
 }
 
 int main (){
-	printf("there are %d ears\n", bunnyEars(4));
+	printf("there are %d ears\n", bunnyEars(5));
 }
