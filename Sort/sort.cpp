25351c29434e6fbc5f0101d07ddd4687ac66e32d
Taly Adler <Redacted>
2025-02-02 16:08:42 +0200

    more work

--- Sort/sort.cpp
+++ Sort/sort.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <array>
 
-int max (int a[]){
+int max (int const a[], int const l) {
     int max = a[0];
-    for(int i = 1; i < 8;i++){
+    for(int i = 1; i < l;i++){
         if (a[i] > max){
             max = a[i];
         }
@@ -10,8 +11,8 @@ int max (int a[]){
     return max;
 }
 
-int search(int a, int v){
-    for (int i = 0; i < 8; i++){
+int search(int const a[], int const v, int const l){
+    for (int i = 0; i < l; i++){
         if (a[i] == v){
             return i;
         }
@@ -19,9 +20,29 @@ int search(int a, int v){
     return -1;
 }
 
+int maxLimited(int a[], int l){
+    int max = -2147483648;
+    for (int i = 0; i < l; i++){
+        if (max <= a[i]) max = a[i];
+    }
+    return max;
+}
+
+int sortedMax(int a[], int l){
+    return a[l-1];
+}
+
+int sort(int& a, int l){
+    int temp[l] = a[];
+    return 0;
+}
+
 int main()
 {
     int a[] = {7,2,10,15,9,3,7,8};
-    std::cout << max(a) << "\n";
+    int l = std::end(a) - std::begin(a);
+    std::cout << "max value in arry is: " << max(a,l) << "\n";
+    std::cout << "max value in index arry is: " << search(a, max(a,l), l) << "\n";
+
     return 0;
 }
