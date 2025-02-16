866334efbcf1a016a4c50c475c2c76bd0aff7e29
Taly Adler <Redacted>
2025-02-16 12:01:07 +0200

    work in progress

--- Sort/sort.cpp
+++ Sort/sort.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
-#include <array>
 
-int max (int const a[], int const l) {
-    int max = a[0];
-    for(int i = 1; i < l;i++){
-        if (a[i] > max){
-            max = a[i];
-        }
+int max (int const a[], int const l);
+
+int search(int const a[], int const v, int const l);
+
+int maxLimited(int a[], int l);
+
+int sortedMax(int a[], int l);
+
+int sort(int& a, int l);
+
+int main()
+{
+    int a[] = {7,2,10,15,9,3,7,8};
+    int size = sizeof(a)/sizeof(a[0]);
+    std::cout << "size of array is \t" << size << "\n";
+    std::cout << "max of array is \t" << max(a,size) << "\n";
+    std::cout << "position of max  is \t" << search(a,max(a,size),size) << "\n";
+    return 0;
+}
+
+int sortedMax(int a[], int l){
+    return a[l-1];
+}
+
+int maxLimited(int a[], int l){
+    if (l <= 0) return -1;
+    int max = -2147483648;
+    for (int i = 0; i < l; i++){
+        if (max <= a[i]) max = a[i];
     }
     return max;
 }
@@ -20,29 +42,22 @@ int search(int const a[], int const v, int const l){
     return -1;
 }
 
-int maxLimited(int a[], int l){
-    int max = -2147483648;
-    for (int i = 0; i < l; i++){
-        if (max <= a[i]) max = a[i];
+int max (int const a[], int const l) {
+    if (l <= 0) return -1;
+    int max = a[0];
+    for(int i = 1; i < l;i++){
+        if (a[i] > max){
+            max = a[i];
+        }
     }
     return max;
 }
 
-int sortedMax(int a[], int l){
-    return a[l-1];
-}
-
-int sort(int& a, int l){
-    int temp[l] = a[];
-    return 0;
-}
-
-int main()
-{
-    int a[] = {7,2,10,15,9,3,7,8};
-    int l = std::end(a) - std::begin(a);
-    std::cout << "max value in arry is: " << max(a,l) << "\n";
-    std::cout << "max value in index arry is: " << search(a, max(a,l), l) << "\n";
-
+int sort(int a[], int l){
+    int temp[l] = a;
+    temp[l-1] = max(a,l);
+    for (int i = l -2; i>0 ;i--){
+        temp[i] = maxLimited(a,temp[i+1]);
+    }
     return 0;
 }
