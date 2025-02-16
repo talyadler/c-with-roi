27858b11b59920f25b4ea317650a50f32510d192
Taly Adler <Redacted>
2025-02-16 14:45:50 +0200

    some anotations

--- Sort/bubble_sort.cpp
+++ Sort/bubble_sort.cpp
@@ -18,9 +18,9 @@ void bubble_sort(int array[], int size){
     for(int i = 0; i < size - 1; i++){
         for(int j = 0; j < size - i - 1; j++){
             if(array[j] > array[j + 1]){
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
+                temp = array[j]; //save value in temp for swap
+                array[j] = array[j + 1]; // swap places for bigger value
+                array[j + 1] = temp; // place the lower value in prev index
             }
         }
     }
