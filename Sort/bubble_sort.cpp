5bfda21a8fe1dc5ae2bb99f6c5807675d9fe7d42
Taly Adler <Redacted>
2025-02-16 14:49:01 +0200

    better array

--- Sort/bubble_sort.cpp
+++ Sort/bubble_sort.cpp
@@ -3,7 +3,7 @@
 void bubble_sort(int array[], int size);
 
 int main() {
-    int array[] = {10, 1, 9, 2, 8, 3, 7, 4, 6, 5};
+    int array[] = {10, -1008, 9, 2, 2, 3, -7, 4, 6, 5};
     int size = 10;
     bubble_sort(array, size);
     for(int i=0; i < size; i++){
