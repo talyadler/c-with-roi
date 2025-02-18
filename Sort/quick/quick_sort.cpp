b16d1bfc25ffe731e0bb5f333c8c8dc096d9073a
Taly Adler <Redacted>
2025-02-18 15:43:00 +0200

    quick_sort by tutorial

---
+++ Sort/quick/quick_sort.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <utility>
+#include <cstdlib>
+#include <ctime>
+
+void quicksort(int a[], int length);
+
+void quicksort_recursion(int a[], int low_index, int high_index);
+
+int partition_index(int a[], int low_index, int high_index);
+
+void print(int a[], int length);
+
+int main(){
+    int a[] = {-5,-8,5,9,88,71,3,100};
+    int length = 8;
+    print(a,length);
+
+    quicksort(a,length);
+
+    print(a,length);
+
+    return 0;
+}
+
+void quicksort(int a[], int length){
+    // initialize with start of array and end of array
+    std::srand(std::time(NULL));
+    quicksort_recursion(a, 0, length - 1);
+}
+
+void quicksort_recursion(int a[], int low_index, int high_index){
+    if (low_index<high_index){
+        int pivot_index = partition_index(a,low_index,high_index);
+        //sort left partition
+        quicksort_recursion(a, low_index, pivot_index - 1);
+        // sort right partition
+        quicksort_recursion(a, pivot_index + 1, high_index);
+    }
+}
+
+int partition_index(int a[], int low_index, int high_index){
+    int pivot_index = low_index +(std::rand() % (high_index - low_index + 1));
+    
+    if (pivot_index != high_index) std::swap(a[pivot_index], a[high_index]);
+
+    int pivot_value = a[high_index];
+    int i = low_index;
+
+    for (int j = low_index; j < high_index; j++){
+        if (a[j] <= pivot_value){
+            std::swap(a[i], a[j]);
+            i++;
+        }
+    }
+
+    std::swap(a[i], a[high_index]);
+    return i;
+}
+
+void print(int a[], int length){
+    for (int i = 0; i < length; i++){
+        std::cout << a[i] << " ";
+    }
+    std::cout << "\n";
+}
