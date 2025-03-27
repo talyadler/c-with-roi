de3f7a9cd201d5215a3ba7d0d49d671a7cc8470c
Taly Adler <redacted>
2025-03-27 14:38:21 -0400

    beautify start

--- generic_list/Array/main.cpp
+++ generic_list/Array/main.cpp
@@ -6,19 +6,23 @@ int main() {
     int c = 5;
     int p = c-c;
 
+    printf("adding %d items to array\n", c);
+
     for (int i = 0; i < c; i++){
         al.addLast(i);
     }
 
+    printf("\ntesting operator []\n", c);
     try{
         for (int i = 0; i <= c; i++){
-            printf("testing index %d: ",i);
+            printf("\ttesting index %d: ",i);
             std::cout << al[i] << "\n";
         }
     } catch (std::out_of_range e) {
         std::cout << e.what() << "\n";
     }
     
-    std::cout << al << "\n";
+    printf("\ntesting operator <<\n", c);
+    std::cout << "\t" << al << "\n";
     return 0;
 }
